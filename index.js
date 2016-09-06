e43e1f9cbfbb1390963c7d09e82f301394143c4f
Mike Bostock <mbostock@gmail.com>
2016-09-06 13:04:56 -0700

    Rename.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import ArraySource from "./array/index";
 import SliceSource from "./slice/index";
 
-export default function(source) {
+export default function slice(source) {
   return new SliceSource(source instanceof ArrayBuffer ? new ArraySource(new Uint8Array(source))
       : source instanceof Uint8Array ? new ArraySource(source)
       : source.getReader());
